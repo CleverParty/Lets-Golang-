34de68cddc9bd7dee5cf1b6f3f4050c337721bca
CleverParty <shanatmail@gmail.com>
2020-05-31 19:41:59 -0400

    small changes

--- keynesSimulation.go
+++ keynesSimulation.go
@@ -7,10 +7,27 @@ import (
 
 func main() {
 
-	var a [10]int
+	a := [3]int{1, 2, 3} // first method
+	var b [3]string      // second method of declaring the arrays
 	fmt.Println(a)
-	for i := 0; i < 10; i++ {
-		a[i] = rand.Intn(100)
-		fmt.Println(a[i])
+	var avg, sumVal int
+	a[0] = rand.Intn(100)
+	a[1] = rand.Intn(100)
+	a[2] = rand.Intn(100)
+	b[0] = "India"
+	b[1] = "Canada"
+	b[2] = "Japan"
+	for index, value := range a {
+		sumVal += value
+		fmt.Printf("a[%d] = %d\n", index, value)
+		if index == 2 {
+			avg = sumVal / 3
+		}
 	}
+	fmt.Println(avg)
+	// for i := 0; i < 10; i++ {
+	//  	a[i] = rand.Intn(100)
+	//	    fmt.Println(a[i])
+	// }
+	fmt.Println("The ratings of the contestants %v:", a)
 }
--- poemProcess.go
+++ poemProcess.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 
 	"gopkg.in/jdkato/prose.v2"
 )
 
 func main() {
 
-	a := [3]int{1, 2, 3}
+	a := [3]int{1, 2, 3} // first method
+	var b [3]string      // second method of declaring the arrays
 	fmt.Println(a)
+	a[0] = rand.Intn(100)
+	a[1] = rand.Intn(100)
+	a[2] = rand.Intn(100)
+	b[0] = "India"
+	b[1] = "Canada"
+	b[2] = "Japan"
 	for index, value := range a {
 		fmt.Printf("a[%d] = %d\n", index, value)
 	}
