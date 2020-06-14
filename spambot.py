3e7e8a3f8e265d011272cd8e6ef6c9a0a019bd3c
CleverParty <shanatmail@gmail.com>
2020-06-13 20:23:10 -0400

    [update] changes made to structure

--- spambot.py
+++ spambot.py
@@ -4,25 +4,21 @@ import asyncio
 from config import *
 import sys
 import asyncio
+import pyautogui
 from bs4 import BeautifulSoup
+
+print('Press Ctrl-C to quit.')
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print("position is :={}".format(positionStr))
+except KeyboardInterrupt:
+     print('\nDone.')
 
 client = discord.Client()
 token = sys.argv[1]
 
+# Get and print the mouse coordinates.
 
-@client.event
-@asyncio.coroutine()
-def on_ready():
-    while not client.is_closed:
-        html = requests.get('https://evilinsult.com/generate_insult.php?lang=en&type=json')
-        soup = BeautifulSoup(html, "html.parser")
-        actual = soup.find('h1')
-        print(actual.text)
-        print(soup)
-        client.send_message(discord.Object(id=DiscordChannel), insult_text.text)
-
-        asyncio.sleep(1)  # Changes how fast the messages are posted. (Anything under 0.7 tends to break it
-
-
-print(token)
-client.run(token, bot=False)
+# permision integer - discord = 1811950599
