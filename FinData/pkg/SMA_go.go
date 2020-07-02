4cf1032788f8c998a25956d18c7b73dacb878b66
CleverParty <shanatmail@gmail.com>
2020-07-02 18:33:59 -0400

    [up/test] figured the go test methodology in relation with unit testing capabilities  unique to golang

--- FinData/pkg/SMA_go.go
+++ FinData/pkg/SMA_go.go
@@ -1,28 +1,12 @@
-package main
+package pkg
 
 import (
 	"fmt"
-	"testing"
 )
 
 type vertex struct { // A struct is a collection of fields.
 	X int
 	Y int
-}
-
-func fact(num int) int {
-	if num > 1 {
-		return num * fact(num-1) // fact test fails but main works
-	} else {
-		return 1
-	}
-}
-
-func TestfactFunc(t *testing.T) {
-	value := fact(3)
-	if value != -3 {
-		t.Errorf("func fact is throwing %d , check out flow again! ", value))
-	}
 }
 
 func main() {
