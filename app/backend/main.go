cd95eaa8ce14d94958fbd9de3ada262b2974f50e
CleverParty <shanatmail@gmail.com>
2020-07-13 21:00:29 -0400

    [update] changes to flow

--- app/backend/main.go
+++ app/backend/main.go
@@ -3,19 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	a := [3]int{1, 2, 3} // edits
-	var avg, sumVal, storeDif int
-	var y, j int
-	a[0] = rand.Intn(100)
-	a[1] = rand.Intn(100)
-	a[2] = rand.Intn(100)
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
