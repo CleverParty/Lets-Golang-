dd8e0fbec136ecc9356b7fe1a087003a7b0ee1e8
CleverParty <shanatmail@gmail.com>
2020-07-15 20:32:11 -0400

    [update] anon refined

--- anon.go
+++ anon.go
@@ -4,11 +4,25 @@ import (
 	"fmt"
 )
 
+func anonFunc() {
+	fmt.Println("the function which accepts other functions as arguements has been entered")
 
-func main(){
-   
+}
+
+func testerFunc(a func()) {
+	// Edit this to test out functions within the environment
+	fmt.Println("the Tester function has been entered")
+}
+
+func main() {
+
+	firstClass := func(a, b int) int {
+		value := a * b
+		return value
+	}
 	anonTest := func() {
 		fmt.Println("the anon function has been executed")
 	}
 	anonTest()
+	fmt.Println("the value returned by first class func is: ", firstClass(12, 34))
 }
