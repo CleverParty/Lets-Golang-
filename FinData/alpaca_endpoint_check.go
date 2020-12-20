bae45c563ba49c29683cdf38ba55d16e83ab2b70
CleverParty <shanatmail@gmail.com>
2020-12-20 14:39:15 -0500

    [update] changes to windows branch

---
+++ FinData/alpaca_endpoint_check.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"fmt"
+	"github.com/alpacahq/alpaca-trade-api-go/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/common"
+	"os"
+)
+func initAlpacaCreds(){
+	valueToBeAsserted = "test"
+	fmt.Println("Value is :",*valueToBeAsserted)
+	alpaca.SetBaseUrl("https://paper-api.alpaca.markets")
+}
+
+func init() {
+	os.Setenv(common.EnvApiKeyID, "")
+	os.Setenv(common.EnvApiSecretKey, "")
+
+	fmt.Printf("Running w/ credentials [%v %v]\n", common.Credentials().ID, common.Credentials().Secret)
+
+	alpaca.SetBaseUrl("https://paper-api.alpaca.markets")
+}
+
+func main() {
+	alpacaClient := alpaca.NewClient(common.Credentials())
+	acct, err := alpacaClient.GetAccount()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(*acct)
+}
+// windows file modification:
+// warning: LF will be replaced by CRLF in test.go.
+// The file will have its original line endings in your working directory
+// warning: LF will be replaced by CRLF in FinData/alpaca_endpoint_check.go.
+// The file will have its original line endings in your working directory
